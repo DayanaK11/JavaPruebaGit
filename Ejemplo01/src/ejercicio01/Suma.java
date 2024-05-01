0f3ad3d9d6ebcfbf8de9523a15b515d947082c1d
DayanaK11 <dayanacayol@gmail.com>
2024-04-30 22:10:00 -0500

    Agragando las dependencias del proyecto y ejercicio suma

---
+++ Ejemplo01/src/ejercicio01/Suma.java
@@ -0,0 +1,13 @@
+package ejercicio01;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("SUMA: ");
+		int a=5,b=10;
+		System.out.println(a+b);
+
+	}
+
+}
